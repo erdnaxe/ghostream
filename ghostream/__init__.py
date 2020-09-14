b0eefa8590c2b65e8b03ae7a75dd46dbf4242e68
Alexandre Iooss <erdnaxe@crans.org>
2020-09-14 15:04:54 +0200

    Lint code

--- ghostream/__init__.py
+++ ghostream/__init__.py
@@ -1,19 +1,22 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, redirect, render_template, request
 import ldap
 
 app = Flask(__name__)
 app.logger.setLevel(20)  # log info
 app.config.from_object('ghostream.default_settings')
 
+
 @app.route('/')
 def index():
     """Welcome page"""
     return render_template('index.html')
 
+
 @app.route('/<path:path>')
 def viewer(path):
     """Show stream that match this path"""
     return render_template('viewer.html', path=path)
+
 
 @app.route('/app/auth', methods=['POST'])
 def auth():
@@ -38,6 +41,6 @@ def auth():
         app.logger.info("%s logged in successfully", name)
         # Remove "?pass=xxx" from RTMP URL
         return redirect(f"rtmp://127.0.0.1:1925/app/{name}", code=302)
-    except:
+    except Exception:
         app.logger.warning("%s failed to log in", name)
         return 'Incorrect credentials', 401
