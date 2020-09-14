ed85463187ce0d702a243c688ece4bf43242a871
Alexandre Iooss <erdnaxe@crans.org>
2020-09-14 12:47:58 +0200

    No more bootstrap, and index page

--- ghostream/default_settings.py
+++ ghostream/default_settings.py
@@ -1,9 +1,11 @@
+import os
+
 # Default configuration
 
 # LDAP
-LDAP_URI = "ldap://127.0.0.1:389"
-LDAP_USER_DN = "cn=users,dc=example,dc=com"
+LDAP_URI = os.environ.get('LDAP_URI') or "ldap://127.0.0.1:389"
+LDAP_USER_DN = os.environ.get('LDAP_USER_DN') or "cn=users,dc=example,dc=com"
 
 # Web page
-SITE_NAME = "Ghostream"
-FAVICON = "/favicon.ico"
+SITE_NAME = os.environ.get('SITE_NAME') or "Ghostream"
+FAVICON = os.environ.get('FAVICON') or "/favicon.ico"
