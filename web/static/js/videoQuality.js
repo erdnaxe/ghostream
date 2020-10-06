24acfe1137a3f204b0660ab220a0a72358d6a1cd
Alexandre Iooss <erdnaxe@crans.org>
2020-10-06 09:12:19 +0200

    Change the quality selector

--- web/static/js/videoQuality.js
+++ web/static/js/videoQuality.js
@@ -1,8 +1,9 @@
-document.getElementsByName("quality").forEach(function (elem) {
-    elem.onclick = function () {
-        // Restart the connection with a new quality
-        peerConnection.createOffer({"iceRestart": true}).then(offer => {
-            return peerConnection.setLocalDescription(offer)
-        }).catch(console.log)
-    }
+document.getElementById("quality").addEventListener("change", (event) => {
+    console.log(`Stream quality changed to ${event.target.value}`)
+
+    // Restart the connection with a new quality
+    // FIXME: set quality
+    peerConnection.createOffer({ "iceRestart": true }).then(offer => {
+        return peerConnection.setLocalDescription(offer)
+    }).catch(console.log)
 })
