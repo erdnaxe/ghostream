34e1e22bf1827532913a4dd264a74f6d5cb40926
Alexandre Iooss <erdnaxe@crans.org>
2020-10-09 22:42:09 +0200

    Add more testing in auth package

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -8,24 +8,32 @@ import (
 )
 
 func TestLoadConfiguration(t *testing.T) {
+	// Test to create a basic authentification backend
 	_, err := New(&Options{
 		Enabled: true,
 		Backend: "basic",
 		Basic:   basic.Options{Credentials: make(map[string]string)},
 	})
-
 	if err != nil {
 		t.Error("Creating basic authentication backend failed:", err)
 	}
 
+	// Test to create a LDAP authentification backend
+	// FIXME Should fail as there is no LDAP server
 	_, err = New(&Options{
 		Enabled: true,
 		Backend: "ldap",
 		LDAP:    ldap.Options{URI: "ldap://127.0.0.1:389", UserDn: "cn=users,dc=example,dc=com"},
 	})
-
-	// TODO Maybe start a LDAP server?
 	if err == nil {
 		t.Error("Creating ldap authentication backend successed mysteriously:", err)
+	}
+
+	// Test to bypass authentification backend
+	backend, err := New(&Options{
+		Enabled: false,
+	})
+	if backend != nil {
+		t.Error("Failed to bypass authentication backend:", err)
 	}
 }
--- auth/basic/basic_test.go
+++ auth/basic/basic_test.go
@@ -8,10 +8,18 @@ func TestBasicLogin(t *testing.T) {
 	basicCredentials := make(map[string]string)
 	basicCredentials["demo"] = "$2b$15$LRnG3eIHFlYIguTxZOLH7eHwbQC/vqjnLq6nDFiHSUDKIU.f5/1H6"
 
+	// Test good credentials
 	backend, _ := New(&Options{Credentials: basicCredentials})
 	ok, err := backend.Login("demo", "demo")
 	if !ok {
 		t.Error("Error while logging with the basic authentication:", err)
 	}
+
+	// Test bad credentials
+	ok, err = backend.Login("demo", "badpass")
+	if ok {
+		t.Error("Authentification failed to fail:", err)
+	}
+
 	backend.Close()
 }
