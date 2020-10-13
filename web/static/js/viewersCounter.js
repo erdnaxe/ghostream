e154fe1a1ead232be183e5983477110d592ece8f
Yohann D'ANELLO <yohann.danello@gmail.com>
2020-10-13 17:43:53 +0200

    Stream ID was broken in the current viewers stats

--- web/static/js/viewersCounter.js
+++ web/static/js/viewersCounter.js
@@ -1,7 +1,7 @@
 // Refresh viewer count by pulling metric from server
-function refreshViewersCounter(period) {
+function refreshViewersCounter(streamID, period) {
     // Distinguish oneDomainPerStream mode
-    fetch("/_stats/" + (location.pathname === "/" ? location.host : location.pathname.substring(1)))
+    fetch("/_stats/" + streamID)
         .then(response => response.json())
         .then((data) => document.getElementById("connected-people").innerText = data.ConnectedViewers)
         .catch(console.log)
